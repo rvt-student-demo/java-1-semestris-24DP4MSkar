04650cbec09959eeb967bea764997e4d48a978b8
24DP4MSkar <A240247MS@rvt.lv>
2025-10-17 12:43:16 +0300

    Chapter60

---
+++ src/main/java/rvt/Chapter60.java
@@ -0,0 +1,41 @@
+package rvt;
+
+public class Chapter60 {
+    public static void main(String[] args) {
+        ex4();
+    }
+    public static void ex1(){
+        int[] val = {0, 1, 2, 3};
+        int sum = 0;
+        for (int i = 0; i < val.length; i = i +1) {
+            sum = sum + i;
+        }
+        System.out.println("Sum of all numbers = " + sum);
+    }
+    public static void ex2(){
+        int[] val = {13, -4, 82, 17};
+        int[] twice = {13, -4, 82, 17};
+
+        System.out.println("Original Array: "+val[0] +" "+val[1]+" "+val[2]+" "+val[3]);
+        for (int i = 0; i < val.length; i = i + 1) {
+            twice[i] *= 2;
+        } 
+        System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+
+    }
+    public static void ex3(){
+    int[] valA   = { 13, -22,  82,  17}; 
+    int[] valB   = {-12,  24, -79, -13};
+    int[] sum    = {  0,   0,   0,   0};
+
+    for(int i = 0; i < valA.length; i = i + 1) {
+        sum[i] = valA[i] + valB[i];
+       }
+    System.out.println( "sum: " + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+    }
+    public static void ex4(){
+        int[] valA   = { 13, -22,  82,  17}; 
+        int[] valB   = {  0,   0,   0,   0};
+        
+    }
+}
